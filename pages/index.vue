ada42bda1f6a9f012035c49bf0cc598940bd8062
tupb <tupb@sudobo.com>
2020-04-08 01:08:56 +0700

    Init project

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        Bắt đầu ở đâu, kết thúc ở đó. <br>
+        Thất bại ở đâu, thất bại tiếp ở đó. <br>
+        <b>In Progress</b><br>
+        <div>
+          <img src="~/assets/images/common/in-progress.gif">
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
